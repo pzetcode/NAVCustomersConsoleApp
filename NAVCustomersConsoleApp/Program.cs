9c81dd1cee8b6543d9a19fe04134d8567a0af068
Pawel Zet <pawel.zalewski.bsc@gmail.com>
2018-06-06 21:27:18 +0200

    Add project files.

---
+++ NAVCustomersConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.OData;
+using Microsoft.OData.Client;
+using Microsoft.OData.Edm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAVCustomersConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NAV nav = new NAV(new Uri("http://localhost:7048/DynamicsNAV100/ODataV4/Company('CRONUS%20CZ%20s.r.o.')"));
+            nav.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;            
+
+            var customers = nav.Customer.Select(c => new { c.Name, c.No});
+            var customersCount = nav.Customer.Count();
+
+            foreach (var cust in customers)
+            {
+                Console.WriteLine("\tCustomer ID: {0}, Name: {1}", cust.No, cust.Name);
+            }
+            Console.WriteLine("There are a total of {0} customers.", customersCount);        
+
+            Console.ReadKey();
+        }
+
+    }
+}
